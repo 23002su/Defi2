771c7a787d1c7a5e9f99a44bb6fe8729af170743
khalive <23090@supnum.mr>
2025-03-17 21:39:05 -1200

    version finale

--- hassaniya/nlp/views.py
+++ hassaniya/nlp/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
-from .nlp_model import process_word
+import pickle
 import os
+from django.shortcuts import render
+from .nlp_model import process_word  # Assurez-vous d'importer la fonction pour traiter les mots
 
-# Définir le chemin du modèle NLP sauvegardé
-MODEL_PATH = os.path.join(os.path.dirname(__file__), 'data', 'nlp_model.pkl')
-
+# Définition du chemin du fichier nlp_model.pkl
+MODEL_PATH = os.path.join(os.path.dirname(__file__), 'data/nlp_model.pkl')
 def transform_word(request):
-    query = request.GET.get('mot', '')  # Récupère le mot saisi dans le formulaire
-    print(f"Mot saisi : {query}")  # Vérifiez que le mot est bien récupéré
+    query = request.GET.get('mot', '').strip()  # Récupérer le mot entré dans l'input
+
+    result = None  # Initialisation du résultat
     
-    result = None  # Initialisation de la variable result
-    
-    if query:  # Si un mot est saisi
-        result = process_word(query, MODEL_PATH)  # Traiter le mot pour obtenir les informations
-    
-    return render(request, 'traduction.html', {'result': result, 'query': query, 'model_path': MODEL_PATH})
+    if query:  # Si un mot a été saisi
+        result = process_word(query, MODEL_PATH)  # Appeler la fonction pour obtenir les résultats à partir du modèle NLP
+        
+        # Ajout de logging pour déboguer
+        print(f"Query: {query}")
+        print(f"Result: {result}")
+
+    # Envoyer les données au template
+    return render(request, 'traduction.html', {'query': query, 'result': result})
+
